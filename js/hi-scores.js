8b945498ac0559be2cea0e27fb073fce451c1038
Brady Davenport <brady.davenport@pm.me>
2022-04-26 17:39:21 -0600

    creating high scores table

--- js/hi-scores.js
+++ js/hi-scores.js
@@ -2,20 +2,76 @@
 
 // ********************** Global Variables ************************************
 
-// username
+let displayedUserName = 'New User';
+if (localStorage.getItem('userName') !== null) {
+  displayedUserName = (localStorage.getItem('userName'));
+} else {
+  displayedUserName;
+}
 
 // ********************** DOM References **************************************
 
 // table
-// username - header
+
+const hiScoresContainer = document.getElementById('high-scores');
+const hiScoresTable = document.createElement('table');
+hiScoresContainer.appendChild(hiScoresTable);
+
+const tBodyElem = document.createElement('tbody');
+hiScoresTable.appendChild(tBodyElem);
+
+
+function createHeaderRow() {
+  const tHeadElem = document.createElement('thead');
+  hiScoresTable.appendChild(tHeadElem);
+
+  const tr = document.createElement('tr');
+  tHeadElem.appendChild(tr);
+
+  const userNameHeading = document.createElement('td');
+  tr.appendChild(userNameHeading);
+  userNameHeading.textContent = 'Player';
+
+  const scoreHeading = document.createElement('td');
+  tr.appendChild(scoreHeading);
+  scoreHeading.textContent = 'Matches Made';
+
+  const timeHeading = document.createElement('td');
+  tr.appendChild(timeHeading);
+  timeHeading.textContent = 'Time Remaining';
+}
+
+function createHighScoreRows() {
+  const highScoreRow = document.createElement('tr');
+  tBodyElem.appendChild(highScoreRow);
+
+  const rowPlayer = document.createElement('td');
+  highScoreRow.appendChild(rowPlayer);
+  rowPlayer.textContent = displayedUserName;
+
+  const rowMatchesMade = document.createElement('td');
+  highScoreRow.appendChild(rowMatchesMade);
+  rowMatchesMade.textContent = 'number of matches made';
+
+  const rowTimeRemaining = document.createElement('td');
+  highScoreRow.appendChild(rowTimeRemaining);
+  rowTimeRemaining.textContent = 'time remaining';
+
+}
+
+createHeaderRow();
+
+for (let i = 0; i < 6; i++) {
+  createHighScoreRows();
+} // placeholder for constructor function, putting scores in from games played
+
+
 
 // ********************** Helpers, Executables ********************************
 
-// check local storage for username
-// username div = username
+let userNameDiv = document.getElementById('username');
+userNameDiv.textContent = displayedUserName;
 
-// adding username-score to high score table
 // highscores = [name, number of cards matched, time] - from local storage
 // current username
 // render highscores data to table
-
